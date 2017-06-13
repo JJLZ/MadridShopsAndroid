c372a35a0d21a3c63bc6dd518475df65714d0b04
Jose Lopez <jjlz.cocoa@gmail.com>
2017-06-13 16:05:11 -0500

    OnClickListener simple implementation

--- app/src/main/java/com/emprendesoft/madridshops/activities/MainActivity.java
+++ app/src/main/java/com/emprendesoft/madridshops/activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.emprendesoft.madridshops.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import com.emprendesoft.madridshops.R;
@@ -20,5 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        shopsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(MainActivity.class.getCanonicalName(), "Hello");
+            }
+        });
+
+        activitiesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(MainActivity.class.getCanonicalName(), "Hello Activities");
+            }
+        });
     }
 }
