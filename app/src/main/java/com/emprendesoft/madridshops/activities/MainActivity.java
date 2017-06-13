c4176192ef1901be2706491c8930e7d1148858ba
Jose Lopez <jjlz.cocoa@gmail.com>
2017-06-13 14:03:44 -0500

    Using ButterKnife

--- app/src/main/java/com/emprendesoft/madridshops/activities/MainActivity.java
+++ app/src/main/java/com/emprendesoft/madridshops/activities/MainActivity.java
@@ -1,15 +1,24 @@
 package com.emprendesoft.madridshops.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.emprendesoft.madridshops.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_main__shops_button) Button shopsButton;
+    @BindView(R.id.activity_main__activities_button) Button activitiesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
