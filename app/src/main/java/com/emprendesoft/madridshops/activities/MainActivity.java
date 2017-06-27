82f36e111a371c3be2139ff00640b9eb91839703
Jose Lopez <jjlz.cocoa@gmail.com>
2017-06-27 14:53:53 -0500

    Background y main thread

--- app/src/main/java/com/emprendesoft/madridshops/activities/MainActivity.java
+++ app/src/main/java/com/emprendesoft/madridshops/activities/MainActivity.java
@@ -9,13 +9,23 @@ import android.widget.Button;
 import com.emprendesoft.madridshops.R;
 import com.emprendesoft.madridshops.navigator.Navigator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.activity_main__shops_button) Button shopsButton;
-    @BindView(R.id.activity_main__activities_button) Button activitiesButton;
+    @BindView(R.id.activity_main__shops_button)
+    Button shopsButton;
+    @BindView(R.id.activity_main__activities_button)
+    Button activitiesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +51,106 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(MainActivity.class.getCanonicalName(), "Hello Activities");
             }
         });
+
+        launchInBackgroundThread();
+    }
+
+    private void launchInBackgroundThread() {
+
+        // onPreExecute
+        // ...
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() { // doInBackground
+                Log.d("HILO", Thread.currentThread().getName());
+                final String s = testMultithread();
+
+                // onPostExecute (going to main thread)
+
+                // method 1
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        shopsButton.setText(s);
+//                    }
+//                });
+
+                // method 2
+//                MainThread.run(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        shopsButton.setText(s);
+//                    }
+//                });
+            }
+        });
+
+        thread.start();
+    }
+
+    private String testMultithread() {
+
+        final String web = "http://freegeoip.net/json/";
+        String result = null;
+
+        try {
+            URL url = new URL(web);
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+
+            InputStream is = (InputStream) request.getContent();
+            result = streamToString(is);
+
+            Log.d("WEB", result);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    String streamToString(InputStream in) throws IOException {
+
+        StringBuilder out = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        for (String line = br.readLine(); line != null; line = br.readLine())
+            out.append(line);
+        br.close();
+
+        return out.toString();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
